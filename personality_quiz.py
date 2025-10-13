18fbdad90414bd781ad222d281cc086e748f19f5
Typhu65 <andykarweit@seattleacademy.org>
2025-10-13 22:59:49 +0000

    newest_submission.py

--- personality_quiz.py
+++ personality_quiz.py
@@ -12,13 +12,13 @@ input("Press Enter to start.")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 answer = input("How would you troll your friends on a Server? (A) Build a obsidian wall around their base? (B) Build a TNT cannon and blow up their base? (C) Kill all their animals? ")
-if answer == " A":
+if answer == "A":
     builder_points += 1
-elif answer == " B":
+elif answer == "B":
     redstoner_points += 1
-elif answer == " C":
+elif answer == "C":
     PVPer_points += 1
-elif answer == " E":
+elif answer == "E":
     Erebos += 1
 else:
     print("Choose A, B, or C next time.")
@@ -29,13 +29,13 @@ input("Press Enter to continue.")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 answer = input("If you had an hour of playing Minecraft what would you do? (A) Build a house? (B) Mine for redstone? (C) Grief a friend's base? ")
-if answer == " A":
+if answer == "A":
     builder_points += 1
-elif answer == " B":
+elif answer == "B":
     redstoner_points += 1
-elif answer == " C":
+elif answer == "C":
     PVPer_points += 1
-elif answer == " r":
+elif answer == "r":
     Erebos += 1
 else:
     print("Choose A, B, or C next time.")
@@ -46,13 +46,13 @@ input("Press Enter to continue.")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 answer = input("If your base was griefed by your friends what would you do? (A) Build a monument where it was? (B) Build a tnt cannon to destroy their base? (C) Hunt them down ruthlessly? ")
-if answer == " A":
+if answer == "A":
     builder_points += 1
-elif answer == " B":
+elif answer == "B":
     redstoner_points += 1
-elif answer == " C":
+elif answer == "C":
     PVPer_points += 1
-elif answer == " e":
+elif answer == "e":
     Erebos += 1
 else:
     print("Choose A, B, or C next time.")
@@ -63,13 +63,13 @@ input("Press Enter to continue.")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 answer = input("What is the first thing you do on a Minecraft server started by your friends? (A) Find a location and build a house? (B) Build a secret base with a redstone door? (C) Just start killing people? ")
-if answer == " A":
+if answer == "A":
     builder_points += 1
-elif answer == " B":
+elif answer == "B":
     redstoner_points += 1
-elif answer == " C":
+elif answer == "C":
     PVPer_points += 1
-elif answer == " b":
+elif answer == "b":
     Erebos += 1
 else:
     print("Choose A, B, or C next time.")
@@ -80,13 +80,13 @@ input("Press Enter to continue.")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 answer = input("How do you give somebody a gift in Minecraft? (A) Build them a house? (B) Build them a vault with a big redstone door? (C) Hunt down their arch-nemisis and bring their armour to them? ")
-if answer == " A":
+if answer == "A":
     builder_points += 1
-elif answer == " B":
+elif answer == "B":
     redstoner_points += 1
-elif answer == " C": 
+elif answer == "C": 
     PVPer_points += 1
-elif answer == " o":
+elif answer == "o":
     Erebos += 1
 else:
     print("Choose A, B, or C next time.")
@@ -97,13 +97,13 @@ input("Press Enter to continue.")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 answer = input("How would you protect your dog in Minecraft? (A) Build a huge fortess for it? (B) Build automated defenses for its doghouse? (C) Kill everyone else so that no one can kill it? ")
-if answer == " A":
+if answer == "A":
     builder_points += 1
-elif answer == " B":
+elif answer == "B":
     redstoner_points
-elif answer == " C":
+elif answer == "C":
     PVPer_points += 1
-elif answer == " s":
+elif answer == "s":
     Erebos += 1
 else:
     print("Choose A, B, or C next time.")
@@ -113,13 +113,13 @@ input("Press Enter to continue.")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 answer = input("Which farm do you like best? (A) A super pretty windmill surrounded by wheat farms? (B) A super complicated kelp farm/auto-smelter? (C) Attack somebody and take over their creeper farm? ")
-if answer == " A":
+if answer == "A":
     builder_points += 1
-elif answer == " B":
+elif answer == "B":
     redstoner_points += 1
-elif answer == " C":
+elif answer == "C":
     PVPer_points += 1
-elif answer == " ." or " 1":
+elif answer == "." or "1":
     Erebos += 1
     print("Erebos Loading...")
 else:
@@ -157,6 +157,7 @@ elif PVPer_points == annoying_points:
     print("You are equally a PVPer and ANNOYING!!! ")
 elif Erebos == 7:
     print("Welcome to Erebos.")
+    
 
 
 
