618e04098ae6b37b4edf2fdaef339b75df3fc94b
Typhu65 <andykarweit@seattleacademy.org>
2025-10-20 18:57:26 +0000

    Code_art_submission_1.py

---
+++ code_art.py
@@ -0,0 +1,98 @@
+import turtle
+import random
+# ###############################################
+
+t = turtle.Turtle()
+t.penup
+t.goto(0,0)
+
+turtle.Screen().bgcolor("Black")
+
+t.speed(10)
+
+
+t.pendown()
+colors = ["blue", "purple",  ]
+
+# First Circle
+
+t.goto(0,0)
+
+for i in range (100):
+    t.forward(40+i)
+    t.left(100)
+    t.color(random.choice(colors))
+    t.forward(100+i)
+    t.color(random.choice(colors))
+    t.left(100)
+
+# Second Circle
+
+t.penup
+t.goto(200,200)
+t.pendown 
+
+for i in range (100):
+    t.forward(40+i)
+    t.left(100)
+    t.color(random.choice(colors))
+    t.forward(100+i)
+    t.color(random.choice(colors))
+    t.left(100)
+
+# Third Circle
+
+t.penup
+t.goto(-200,200)
+t.pendown 
+
+for i in range (100):
+    t.forward(40+i)
+    t.left(100)
+    t.color(random.choice(colors))
+    t.forward(100+i)
+    t.color(random.choice(colors))
+    t.left(100)
+
+t.penup
+t.goto(-200,-200)
+t.pendown 
+
+for i in range (100):
+    t.forward(40+i)
+    t.left(100)
+    t.color(random.choice(colors))
+    t.forward(100+i)
+    t.color(random.choice(colors))
+    t.left(100)
+
+t.penup
+t.goto(200,-200)
+t.pendown 
+
+for i in range (100):
+    t.forward(40+i)
+    t.left(100)
+    t.color(random.choice(colors))
+    t.forward(100+i)
+    t.color(random.choice(colors))
+    t.left(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.exitonclick ()
+
+
+
+
